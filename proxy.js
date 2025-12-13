aa7605b828efc59f41392cf51c3d65ec0bb936ea
UltraV <1216564567@qq.com>
2025-12-13 10:24:05 +0800

    fix: playground not in protected routes

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
-const isProtectedRoute = createRouteMatcher(['/prompts(.*)'])
+const isProtectedRoute = createRouteMatcher(['/prompts(.*)', '/playground(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
